287e087d2a168fa2434ada5d40b6309da6157277
Jay Cuevas <Jordan.Cuevas@precisecal.com>
2021-09-25 14:46:37 -0400

    Moved client to async version

--- client.py
+++ client.py
@@ -1,6 +1,20 @@
 import socketio
+import asyncio
 
-client = socketio.Client()
-client.connect('http://localhost:8080',auth={'username':'client1'})
-client.emit('chat_message', "here is the message")
-client.disconnect()
+
+client = socketio.AsyncClient()
+
+
+@client.event
+def chat_message(data):
+    print("Received message: ", data)
+
+
+async def main():
+    await client.connect('http://localhost:8080', auth={'username': 'client1'})
+
+    await client.emit('chat_message', "here is the message")
+    await client.wait()
+
+if __name__ == "__main__":
+    asyncio.run(main())
